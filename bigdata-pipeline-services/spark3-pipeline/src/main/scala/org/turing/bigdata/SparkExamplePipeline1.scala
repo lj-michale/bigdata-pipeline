ddbf0b36d206a2bd85b100048230945ac5cc342d
Tony <economic_luojie@outlook.com>
2024-01-23 15:13:24 +0800

    feat: SparkExamplePipeline1

--- bigdata-pipeline-services/spark3-pipeline/src/main/scala/org/turing/bigdata/SparkExamplePipeline1.scala
+++ bigdata-pipeline-services/spark3-pipeline/src/main/scala/org/turing/bigdata/SparkExamplePipeline1.scala
@@ -1,16 +1,29 @@
 package org.turing.bigdata
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
 /**
  * @descri:
  *
  * @author: lj.michale
- * @date: 2024/1/19 15:06
+ * @date: 2024/1/23 10:33
  */
 object SparkExamplePipeline1 {
 
   def main(args: Array[String]): Unit = {
 
-    println(" >>>>>>>>>>>>>>>>> ")
+    Logger.getLogger("org").setLevel(Level.OFF)
+    val spark = SparkSession
+      .builder
+      .master("local[*]")
+      .appName("SparkSQLExample")
+      .getOrCreate()
+
+    println(" >>>>>>>>>>>>>>>>>>>>>>>>> ")
+
+    spark.stop()
+
 
   }
 
