d543c574be711b2197ae1b54bfb1f44f8b9a40aa
Tony <economic_luojie@outlook.com>
2024-01-23 15:11:01 +0800

    feat: README.md

---
+++ bigdata-pipeline-services/spark3-pipeline/src/main/scala/org/turing/bigdata/SparkExamplePipeline1.scala
@@ -0,0 +1,17 @@
+package org.turing.bigdata
+
+/**
+ * @descri:
+ *
+ * @author: lj.michale
+ * @date: 2024/1/19 15:06
+ */
+object SparkExamplePipeline1 {
+
+  def main(args: Array[String]): Unit = {
+
+    println(" >>>>>>>>>>>>>>>>> ")
+
+  }
+
+}
