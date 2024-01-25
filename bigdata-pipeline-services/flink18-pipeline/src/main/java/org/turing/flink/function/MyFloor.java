ff158771c270380d716e9e6d92ff91f7a7da9beb
Tony <economic_luojie@outlook.com>
2024-01-25 15:08:16 +0800

    feat: flink|flink1.18.1|Table API + Table API + DataStream Scala 版 -> flink-table-api-java + link-table-api-java-bridge

---
+++ bigdata-pipeline-services/flink18-pipeline/src/main/java/org/turing/flink/function/MyFloor.java
@@ -0,0 +1,17 @@
+package org.turing.flink.function;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.functions.ScalarFunction;
+
+public class MyFloor extends ScalarFunction {
+
+    public @DataTypeHint("TIMESTAMP(3)") LocalDateTime eval(
+            @DataTypeHint("TIMESTAMP(3)") LocalDateTime timestamp) {
+
+        return timestamp.truncatedTo(ChronoUnit.HOURS);
+    }
+
+}
