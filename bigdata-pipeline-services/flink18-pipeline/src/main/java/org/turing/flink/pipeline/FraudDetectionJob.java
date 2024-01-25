87a20b3052cd3ba7ad531629758320dcb46a878e
Tony <economic_luojie@outlook.com>
2024-01-25 15:01:26 +0800

    feat: flink|flink1.18.1|Flink本地测试WebUI依赖&Flink本地测试客户端依赖

--- bigdata-pipeline-services/flink18-pipeline/src/main/java/org/turing/flink/pipeline/FraudDetectionJob.java
+++ bigdata-pipeline-services/flink18-pipeline/src/main/java/org/turing/flink/pipeline/FraudDetectionJob.java
@@ -10,10 +10,11 @@ import org.apache.flink.walkthrough.common.entity.Alert;
 import org.apache.flink.walkthrough.common.entity.Transaction;
 import org.apache.flink.walkthrough.common.source.TransactionSource;
 import org.turing.flink.function.FraudDetector;
+import org.turing.flink.function.FraudDetector2;
 
 /**
  * @descri: 反欺诈作业示例
- *
+ *          flink web ui 访问URL: localhost:8081
  * @author: lj.michale
  * @date: 2024/1/25 11:20
  */
@@ -22,6 +23,7 @@ public class FraudDetectionJob {
     public static void main(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
+        conf.setInteger("rest.port", 8081);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
         // checkpoint配置
         env.enableCheckpointing(5000);
@@ -31,6 +33,7 @@ public class FraudDetectionJob {
         env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
         // 状态后端
         env.setStateBackend(new HashMapStateBackend());
+        env.getCheckpointConfig().setCheckpointStorage("file:///E:\\OpenSource\\SourceCode\\bigdata\\bigdata-pipeline\\checkpoints");
 
         DataStream<Transaction> transactions = env.addSource(new TransactionSource())
                 .name("transactions");
