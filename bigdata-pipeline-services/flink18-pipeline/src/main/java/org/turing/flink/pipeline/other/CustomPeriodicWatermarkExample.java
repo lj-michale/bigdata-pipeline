6280feadf9f582cc4288ad4c646f5e621d9b103e
Tony <economic_luojie@outlook.com>
2024-01-26 16:10:51 +0800

    feat: flink|flink1.18.1|周期性水位线生成器（Periodic Generator）

--- bigdata-pipeline-services/flink18-pipeline/src/main/java/org/turing/flink/pipeline/other/CustomPeriodicWatermarkExample.java
+++ bigdata-pipeline-services/flink18-pipeline/src/main/java/org/turing/flink/pipeline/other/CustomPeriodicWatermarkExample.java
@@ -1,15 +1,65 @@
 package org.turing.flink.pipeline.other;
 
-
 import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.turing.flink.bean.Event;
+import org.turing.flink.source.ClickSource;
 
 /**
- * @descri: 
- *
+ * @descri: 周期性水位线生成器（Periodic Generator）
+ * 周期性生成器一般是通过onEvent()观察判断输入的事件，而在onPeriodicEmit()里发出水位线。
+ * 我们在onPeriodicEmit()里调用output.emitWatermark()，就可以发出水位线了；这个方法由系统框架周期性地调用，默认200ms一次。
+ * 如果想修改默认周期时间，可以通过下面方法修改。例如：修改为400ms
  * @author: lj.michale
  * @date: 2024/1/26 13:03
  */
+
 public class CustomPeriodicWatermarkExample {
 
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.addSource(new ClickSource())
+                .assignTimestampsAndWatermarks(new CustomWatermarkStrategy())
+                .print();
+
+        env.execute();
+
+    }
+
+    public static class CustomWatermarkStrategy implements WatermarkStrategy<Event> {
+
+        @Override
+        public TimestampAssigner<Event> createTimestampAssigner(TimestampAssignerSupplier.Context context) {
+            return new SerializableTimestampAssigner<Event>() {
+                @Override
+                public long extractTimestamp(Event element, long recordTimestamp) {
+                    return element.getTimestamps(); // 告诉程序数据源里的时间戳是哪一个字段
+                }
+            };
+        }
+
+        @Override
+        public WatermarkGenerator<Event> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
+            return new CustomBoundedOutOfOrdernessGenerator();
+        }
+    }
+
+    public static class CustomBoundedOutOfOrdernessGenerator implements WatermarkGenerator<Event> {
+        private Long delayTime = 5000L; // 延迟时间
+        private Long maxTs = -Long.MAX_VALUE + delayTime + 1L; // 观察到的最大时间戳
+
+        @Override
+        public void onEvent(Event event, long eventTimestamp, WatermarkOutput output) {
+            // 每来一条数据就调用一次
+            maxTs = Math.max(event.getTimestamps(), maxTs); // 更新最大时间戳
+        }
+
+        @Override
+        public void onPeriodicEmit(WatermarkOutput output) {
+            // 发射水位线，默认200ms调用一次
+            output.emitWatermark(new Watermark(maxTs - delayTime - 1L));
+        }
+    }
 }
+
